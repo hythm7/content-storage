16c10955d7cfb7316031cde7c22519646b118648
Haytham Elganiny <elganiny.haytham@gmail.com>
2024-05-07 23:37:57 -0400

    Style

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,7 @@ module.exports = {
 	},
 	output: {
 		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'static'),
-		clean: true
+		path: path.resolve(__dirname, 'static')
 	},
 	plugins: [
 		new miniCssExtractPlugin()
